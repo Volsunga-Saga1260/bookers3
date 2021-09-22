82b9411d13a56c80b00e863e999b5e0272480e06
Volsunga-Saga1260 <longinus1226@gmail.com>
2021-09-22 12:08:04 +0000

    index作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,9 @@ class BooksController < ApplicationController
   def new
     @book = current_customer.books.new
   end
+  
+  def index
+    @movies = Movie.all.page(params[:page]).reverse_order
+  end
+
 end
