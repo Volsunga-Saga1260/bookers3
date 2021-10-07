f456e506b328f87f4190700f70a78ca88473ad18
Volsunga-Saga1260 <longinus1226@gmail.com>
2021-10-07 12:55:12 +0000

    README.md編集

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,3 +8,23 @@ class BooksController < ApplicationController
   end
 
 end
+
+<li class="nav-list-item">
+            <%= link_to user_path(current_user.id) do %>
+              <i class="fas fa-home" aria-hidden="true"></i>
+              Home
+            <% end %>
+          </li>
+          
+          <li class="nav-list-item">
+            <%= link_to users_path do %>
+              <i class="fas fa-users" aria-hidden="true"></i>
+              Users
+            <% end %>
+          </li>
+          
+          <% if gadget.user == current_user %>
+                  <%= link_to gadget.user.name, users_path(current_user) %>
+                <% else %>
+                  <%= link_to gadget.user.name, user_path(gadget.user.id) %>
+                <% end %>
